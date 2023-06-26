c886d991cb09b7fce669acc31bab7d1f75b44030
hanhojjung <ghwjd9269@naver.com>
2023-06-26 15:33:32 +0900

    230626 6교시

--- ch02/Code2-07_test.py
+++ ch02/Code2-07_test.py
@@ -4,8 +4,13 @@ import random
 # 함수 선언 부분
 def screenLeftClick(x,y) : 
     global r,g,b
+    rAngle = random.randrange(1,360)
+    tSize = random.randrange(1,10)
+    turtle.shapesize(tSize)
     turtle.pencolor((r,g,b)) 
     turtle.pendown()
+    turtle.right(rAngle)
+    turtle.stamp()
     turtle.goto(x,y)
 
 def screenRightClick(x,y) : 
@@ -27,6 +32,14 @@ pSize = 8
 r,g,b = 0.0, 0.0, 0.0
 
 # 메인 코드 부분 
+image_path = "c:/san1.png"
+screen.register_shape(image_path)
+
+screen = turtle.Screen()
+image1 = "san1.png"
+# screen.addshape(image1)
+screen.register_shape(image1)
+
 turtle.title('거북이로 그림 그리기')
 turtle.shape('classic')
 turtle.pensize(pSize)
@@ -42,8 +55,9 @@ turtle.onscreenclick(screenRightClick,3)
 # turtle.circle(70)
 # turtle.end_fill()
 
-turtle.color("black", "red")
-turtle.begin_fill()
-turtle.circle(80)
-turtle.end_fill()
+# turtle.color("black", "red")
+# turtle.begin_fill()
+# turtle.circle(80)
+# turtle.end_fill()
+
 turtle.done()
